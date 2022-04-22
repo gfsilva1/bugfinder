81a71f5961e84266cef9de0b31893bc30f6b9735
Guilherme Silva <gfsilva@hotmail.com>
2022-04-22 14:59:19 -0300

    rev

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -1,14 +1,16 @@
 class ParserController < ApplicationController
+require "csv"
+
   def index
   end
 
   def parse
-    require "csv"
+    movies = []
+    filepath = params["csv"]
 
-    filepath = params["csv"].to_s
-
-    CSV.foreach(filepath, headers: :first_row) do |row|
-    puts "#{row['First Name']} #{row['Last Name']} played #{row['Instrument']}"
+    CSV.foreach(filepath) do |row|
+      movie = row[1].to_o
     end
+    movie
   end
 end
